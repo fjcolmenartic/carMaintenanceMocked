8f9934e4b818de335f0ac97a988db973bce7a665
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-07 12:14:15 +0200

    Adding initial content pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
+import { CarComponent } from './content/car/car.component';
+import { DashboardComponent } from './content/dashboard/dashboard.component';
+import { LoginComponent } from './content/login/login.component';
+import { MaintenanceComponent } from './content/maintenance/maintenance.component';
+import { NotFoundComponent } from './content/not-found/not-found.component';
+import { RepairComponent } from './content/repair/repair.component';
+import { UserComponent } from './content/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    CarComponent,
+    DashboardComponent,
+    LoginComponent,
+    MaintenanceComponent,
+    NotFoundComponent,
+    RepairComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
