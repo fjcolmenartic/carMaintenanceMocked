680d19befdd9c20b51d9298efe635f9ce380f4bd
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-07 12:16:21 +0200

    Adding initial routes for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './content/car/car.component';
+import { DashboardComponent } from './content/dashboard/dashboard.component';
+import { LoginComponent } from './content/login/login.component';
+import { MaintenanceComponent } from './content/maintenance/maintenance.component';
+import { NotFoundComponent } from './content/not-found/not-found.component';
+import { RepairComponent } from './content/repair/repair.component';
+import { UserComponent } from './content/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'car', component: CarComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'maintenances', component: MaintenanceComponent },
+  { path: 'repairs', component: RepairComponent },
+  { path: 'logout', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
