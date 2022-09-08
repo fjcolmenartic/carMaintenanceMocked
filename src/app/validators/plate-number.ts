433ca01fdb20341f66b61e689d98595b08960c52
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-08 09:32:41 +0200

    Validator for plate number format

--- src/app/validators/plate-number.ts
+++ src/app/validators/plate-number.ts
@@ -4,7 +4,40 @@ import { AbstractControl, ValidationErrors, Validator } from "@angular/forms";
 @Injectable({providedIn: 'root'})
 export class PlateNumber implements Validator {
     validate(control: AbstractControl): ValidationErrors | null {
-        throw new Error("Method not implemented.");
+        
+        // Get the value
+        let plateNumber = control.value;
+
+        // Regex format for modern plate number since 2000 in advanced
+        let modernPlate = /^\d{4}[BCDFGHJKLMNPRSTVWXYZ]{3}$/;
+
+        // Guess if the whole plate number is in the exact range
+        if (plateNumber.toString().length == 7) {
+            // Split the parts of the plate number
+            const plateNumberNumbers:number = parseInt(plateNumber.substr(0,4));
+            const plateNumberLetters:string = plateNumber.substr(4,3);
+
+            // Check if plate number is in valid format (exact size of each part and regex format)
+            if (
+                typeof plateNumberNumbers == 'number' &&
+                plateNumberNumbers.toString().length == 4 && 
+                plateNumberLetters.length == 3 &&
+                modernPlate.test(plateNumber)
+            ) {
+                return null;
+            } else {
+                return { modernPlateNumberNoValidLetter: false };
+            }
+        } else {
+            return { modernPlateNumberWrongLongitude: false };
+        }
+        
     }
 
 }
+
+
+
+
+
+
