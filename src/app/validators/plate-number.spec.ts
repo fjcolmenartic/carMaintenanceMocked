27de44c4e1e36b722fd668c9ff462c0042ae19d9
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-08 10:31:49 +0200

    Testings for plate number validator

--- src/app/validators/plate-number.spec.ts
+++ src/app/validators/plate-number.spec.ts
@@ -1,7 +1,51 @@
+import { FormControl } from '@angular/forms';
 import { PlateNumber } from './plate-number';
 
 describe('PlateNumber', () => {
+
   it('should create an instance', () => {
     expect(new PlateNumber()).toBeTruthy();
   });
+
+  it('Check for a valid plate number', () => {
+    let validator = new PlateNumber();
+    let plateNumber: FormControl;
+
+    plateNumber = new FormControl('1234TSJ');
+    let validation = validator.validate(plateNumber);
+
+    expect(validation).toBeNull();
+  });
+
+  it('Check for invalid plate number - wrong order', () => {
+    let validator = new PlateNumber();
+    let plateNumber: FormControl;
+
+    plateNumber = new FormControl('DVG1562');
+    console.log(plateNumber.value.length)
+    let validation = validator.validate(plateNumber);
+
+    expect(validation).toEqual({ modernPlateNumberNoValidLetter: false });
+  });
+
+  it('Check for invalid plate number - out of range of 7 chars', () => {
+    let validator = new PlateNumber();
+    let plateNumber: FormControl;
+
+    plateNumber = new FormControl('1234TS');
+    let validation = validator.validate(plateNumber);
+
+    expect(validation).toEqual({ modernPlateNumberWrongLongitude: false });
+  });
+
+  it('Check for invalid plate number - in range of 7 chars BUT not valid format (length or position of numns/chars', () => {
+    let validator = new PlateNumber();
+    let plateNumber: FormControl;
+
+    plateNumber = new FormControl('12R34TS');
+    let validation = validator.validate(plateNumber);
+
+    expect(validation).toEqual({ modernPlateNumberNoValidLetter: false });
+  });
+
 });
