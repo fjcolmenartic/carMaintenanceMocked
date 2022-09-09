a3736e60219ecfa8687c6978fb989cf961b1ce03
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-09 19:16:44 +0200

    New links 8temporary) for developing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { MaintenanceComponent } from './content/maintenance/maintenance.componen
 import { NotFoundComponent } from './content/not-found/not-found.component';
 import { RegisterComponent } from './content/register/register.component';
 import { RepairComponent } from './content/repair/repair.component';
+import { SetCarComponent } from './content/set-car/set-car.component';
+import { SetMaintenanceComponent } from './content/set-maintenance/set-maintenance.component';
+import { SetRepairComponent } from './content/set-repair/set-repair.component';
+import { SetUserComponent } from './content/set-user/set-user.component';
 import { UserComponent } from './content/user/user.component';
 
 export const routes: Routes = [
@@ -18,6 +22,12 @@ export const routes: Routes = [
   { path: 'repairs', component: RepairComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'logout', component: LoginComponent },
+
+  { path: 'set-car', component: SetCarComponent },
+  { path: 'set-maintenance', component: SetMaintenanceComponent },
+  { path: 'set-repair', component: SetRepairComponent },
+  { path: 'set-user', component: SetUserComponent },
+
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
