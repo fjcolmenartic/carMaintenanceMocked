7656cb19c4c86b7fec4e6fd5bde6748d4f657a8e
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-09 12:56:17 +0200

    Update dependencies

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ListBoxComponent } from './list-box/list-box.component';
+import { TextBoxComponent } from './text-box/text-box.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     // NavigationBarComponent
+  
+    ListBoxComponent,
+    TextBoxComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     // NavigationBarComponent
