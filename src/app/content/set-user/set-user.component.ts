f964ead81fe3c265d5f6e080b75ca12e046f2f24
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-09 12:51:17 +0200

    adding set user for insert - edit crud

--- src/app/content/set-user/set-user.component.ts
+++ src/app/content/set-user/set-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
+import { StorageService } from 'src/app/services/storage.service';
+import { PasswordsMatch } from 'src/app/validators/passwords-match';
 
 @Component({
   selector: 'app-set-user',
@@ -6,10 +10,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./set-user.component.css']
 })
 export class SetUserComponent implements OnInit {
+  title = "Registro";
+  //registerForm: FormGroup = new FormGroup({});
+  dataSession: any;
+  isCheck: any;
+  checkHuman: Array<any> =  [];
+  registerSuccess = false;
+  //PasswordsMatch: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null | undefined;
 
-  constructor() { }
+  // name, email, password, city
+  registerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(15),
+      Validators.pattern(/^[A-Za-z]+$/ )
+    ]),
+    email:new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(3),
+      Validators.maxLength(20)
+    ]),
+    password:new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(15)
+    ]),
+    confirmPassword:new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(15)
+    ]),
+    city:new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-zA-Z -ÁÉÍÓÚáéíóúÄËÏÖÜäëïöü]+\D[^/\()@!¡"·$%&()=+*\^{}\[_ªº\]]$/)
+    ])
+  });
+
+  constructor(
+    private dataService: DataService,
+    private storageService: StorageService,
+    private PasswordsMatch: PasswordsMatch
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(e:any) {
+
+    let passwordsMatch = this.PasswordsMatch.validate(
+        this.registerForm.controls['password'].value, 
+        this.registerForm.controls['confirmPassword'].value);
+
+    if(!!!passwordsMatch) {
+
+      let name = this.registerForm.controls['name'].value;
+      let email = this.registerForm.controls['email'].value;
+      let password = this.registerForm.controls['password'].value;
+      let confirmPassword = this.registerForm.controls['confirmPassword'].value;
+
+      // this.signinService.register(
+      //   name, email, password, confirmPassword
+      //   )
+      //   .subscribe(
+      //     res => {
+      //       this.dataSession = res;
+      //       console.info('register SUCCESS')
+      //       this.registerSuccess = true;
+      //     },
+      //     (err: any) => {
+      //       console.warn('ERROR ON POST - REGISTER')
+      //     });
+
+          // todo toast success
+
+    } else {
+      // TODO retornar un toast ???
+    }
+
+   
   }
 
 }
