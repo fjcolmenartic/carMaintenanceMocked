25768439ead6c5d29d0f7068a100cd90ceed57eb
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-09 12:54:06 +0200

    Initial register form, makes post request,

--- src/app/content/register/register.component.ts
+++ src/app/content/register/register.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DataService } from 'src/app/services/data.service';
+import { SessionService } from 'src/app/services/session.service';
 import { SignInService } from 'src/app/services/sig-in.service';
 import { StorageService } from 'src/app/services/storage.service';
-import { PlateNumber } from 'src/app/validators/plate-number';
 import { PasswordsMatch } from 'src/app/validators/passwords-match';
 
 @Component({
@@ -15,8 +15,11 @@ export class RegisterComponent implements OnInit {
 
   title = "Registro";
   //registerForm: FormGroup = new FormGroup({});
+  dataSession: any;
   isCheck: any;
   checkHuman: Array<any> =  [];
+  registerSuccess = false;
+  
   //PasswordsMatch: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null | undefined;
 
   // name, email, password, city
@@ -49,17 +52,15 @@ export class RegisterComponent implements OnInit {
       Validators.maxLength(20),
       Validators.pattern(/^[a-zA-Z -ÁÉÍÓÚáéíóúÄËÏÖÜäëïöü]+\D[^/\()@!¡"·$%&()=+*\^{}\[_ªº\]]$/)
     ])
-  }
-
-  );
+  });
 
 
   constructor(
-    private dataService: DataService,
-    private storageService: StorageService,
-    private plateNumber: PlateNumber,
+    private dataService: DataService, // Captcha
+    private storageService: StorageService, // session navegador
     private signinService: SignInService,
-    private PasswordsMatch: PasswordsMatch
+    private PasswordsMatch: PasswordsMatch,
+    private sessionService: SessionService
   ) { }
 
   ngOnInit(): void {
@@ -67,6 +68,41 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(e:any) {
 
+    let passwordsMatch = this.PasswordsMatch.validate(
+        this.registerForm.controls['password'].value, 
+        this.registerForm.controls['confirmPassword'].value);
+
+        console.log('####', this.registerForm, this.registerForm.controls)
+        // todo implements validator and sent this.registerForm.controls
+        debugger;
+
+    if(!!!passwordsMatch) {
+
+      let name = this.registerForm.controls['name'].value;
+      let email = this.registerForm.controls['email'].value;
+      let password = this.registerForm.controls['password'].value;
+      let confirmPassword = this.registerForm.controls['confirmPassword'].value;
+
+      this.signinService.register(
+        name, email, password, confirmPassword
+        )
+        .subscribe(
+          res => {
+            this.dataSession = res;
+            console.info('register SUCCESS')
+            this.registerSuccess = true;
+          },
+          (err: any) => {
+            console.warn('ERROR ON POST - REGISTER')
+          });
+
+          // todo toast success
+
+    } else {
+      // TODO retornar un toast ???
+    }
+
+   
   }
 
 }
