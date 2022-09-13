f670c8e9333f426e2af3f30c4466dd3567247801
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-13 18:20:48 +0200

    Adding the initial develope of view and business

--- src/app/content/repair/repair.component.ts
+++ src/app/content/repair/repair.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-repair',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepairComponent implements OnInit {
 
-  constructor() { }
+  title = 'Listado de reparaciones';
+  repairList: any;
+  dataSession: any;
+  isCheck: any;
+  userSession = false;
+
+  constructor(
+    private sessionService: SessionService,
+    private storageService: StorageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    // Get the user id from storage
+    let userId = JSON.parse(this.sessionService.getData('user-id') || ' {}');
+    userId = userId.toString();
+    // Get the user data
+    this.storageService.getAllRepairs(userId)
+    .subscribe(
+      res => {
+        this.dataSession = res;
+        this.repairList = res;
+        console.warn(res)
+        this.isCheck = 'SUCCESS';
+
+      },
+      (err: any) => {
+        this.isCheck = 'ERROR_USER';
+      });
+
   }
 
 }
