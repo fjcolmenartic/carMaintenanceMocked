db5255cb0aadc3a494c01e91c170f411399255c9
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-13 15:39:49 +0200

    New car registration DONE

--- src/app/content/set-car/set-car.component.ts
+++ src/app/content/set-car/set-car.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SessionService } from 'src/app/services/session.service';
+import { StorageService } from 'src/app/services/storage.service';
 import { PlateNumber } from 'src/app/validators/plate-number';
 
 @Component({
@@ -12,6 +14,10 @@ export class SetCarComponent implements OnInit {
   title = 'Editar coche';
   colorList = ['Blanco', 'Negro', 'Gris', 'Azul', 'Rojo', 'Verde', 'Granate', 'Amarillo', 'Rosa', 'Beige'];
   carTypeList = ['Diesel', 'Gasolina', 'Eléctrico', 'Híbrido', 'GLP'];
+  yearList: string[] = [];
+  dataSession: any;
+  isCheck: any;
+
   // name, email, password, city
   setACar = new FormGroup({
     plateNum: new FormControl('', [
@@ -20,7 +26,7 @@ export class SetCarComponent implements OnInit {
       Validators.maxLength(7),
       this.plateNumber.validate
     ]),
-    branch:new FormControl('', [
+    brand:new FormControl('', [
       Validators.required,
       Validators.pattern(/^[A-Za-z]+$/),
       Validators.minLength(3),
@@ -28,8 +34,8 @@ export class SetCarComponent implements OnInit {
     ]),
     model:new FormControl('', [
       Validators.required,
-      Validators.pattern(/^[A-Za-z0-9]+[`!_,¿?=)(/&%$·"ªº\\^*+)]$/),
-      Validators.minLength(5),
+      Validators.pattern(/^[A-Za-z0-9]+$/),
+      Validators.minLength(3),
       Validators.maxLength(15)
     ]),
     color:new FormControl('', [
@@ -67,23 +73,72 @@ export class SetCarComponent implements OnInit {
       Validators.minLength(3),
       Validators.maxLength(20),
       Validators.pattern(/^[\d.]+$/) // diesel, gaslina....
-    ]),
-    userId:new FormControl('', [
-      Validators.required,
-      Validators.minLength(1),
-      Validators.maxLength(20),
-      Validators.pattern(/^[\d]+$/)
     ])
   });
 
   constructor(
-    private plateNumber: PlateNumber
+    private plateNumber: PlateNumber,
+    private sessionService: SessionService,
+    private storageService: StorageService
   ) { }
 
   ngOnInit(): void {
+
+    // Create an populate a year list for the input year select
+    let iniYear: number = 1900;
+    let lastYear: any = new Date((new Date()).getTime() + 24*60*60*1000);
+    lastYear = lastYear.getFullYear();
+
+    for(let i = lastYear; i >= iniYear; i--) {
+      this.yearList.push(i.toString());
+    }
+
   }
 
   onSubmit(e: any) {
+
+    let plateNum = this.setACar.controls['plateNum'].value;
+    let brand = this.setACar.controls['brand'].value;
+    let model = this.setACar.controls['model'].value;
+    let color = this.setACar.controls['color'].value;
+    let doors = this.setACar.controls['doors'].value;
+    let type = this.setACar.controls['type'].value;
+    let kilometers = this.setACar.controls['kilometers'].value;
+    let year = this.setACar.controls['year'].value;
+    let engine = this.setACar.controls['engine'].value;
+    let userId = JSON.parse(this.sessionService.getData('user-id') || ' {}');
+    let id = 0;
+
+    if(
+        plateNum != '' &&
+        brand != '' &&
+        model != '' &&
+        color != '' &&
+        doors != '' &&
+        type != '' &&
+        kilometers != '' &&
+        year != '' &&
+        engine != ''     
+    ) {
+      // Update user data
+      this.storageService
+        .setCar(plateNum, brand, model, color, doors, type, kilometers,year, engine, userId, id)
+        .subscribe(
+          res => {
+            this.dataSession = res;
+            this.isCheck = 'INSERT_SUCCESS';
+            console.warn(this.isCheck)    
+          },
+          (err: any) => {
+            this.isCheck = 'INSERT_ERROR'; 
+            console.error(this.isCheck)
+          });
+
+    } else {
+      // TODO retornar un toast ???
+      this.isCheck = 'EMPTY_FIELDS_ERROR';
+      console.error(this.isCheck)
+    }
 
   }
 
