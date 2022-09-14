00d373734e278a6633d8eba7bc9a0a699185db46
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-14 12:06:21 +0200

    New dropdown menu + input for app title from parent

--- src/app/shared/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { SessionStatusService, UserSession } from 'src/app/services/session-status.service';
+import { SessionStatusService } from 'src/app/services/session-status.service';
 import { SessionService } from 'src/app/services/session.service';
 
 @Component({
@@ -9,29 +8,22 @@ import { SessionService } from 'src/app/services/session.service';
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css']
 })
-export class NavigationBarComponent implements OnInit, OnChanges {
+export class NavigationBarComponent implements OnInit {
 
   // Value from parent if session started on browser or not
   @Input() userSession = false;
   @Input() userName = '';
-  @Output() eventData = new EventEmitter<boolean>();
-
-  data$: Observable<UserSession>;
-
+  @Input() appTitle = '';
+  @Output() eventData = new EventEmitter<object>();
   
   constructor(
     private sessionService: SessionService,
     private router: Router,
-    private sessionStatus: SessionStatusService,
-
   ) {
-    this.data$ = sessionStatus.sessionStatusObservable;
-
    }
 
   ngOnInit(): void {
     let sessionStarted = this.sessionService.getData('user-session');
-    console.log('sessionstarted', sessionStarted)
 
     // If session starte update userSession value
     if(sessionStarted) {
@@ -40,20 +32,12 @@ export class NavigationBarComponent implements OnInit, OnChanges {
       this.userSession = false;
       this.router.navigateByUrl('/login');
     }
-  }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.data$ = this.sessionStatus.sessionStatusObservable;
   }
 
   logout() {
-    // Remove browser session
-    // this.sessionService.removeData('user-session');
-    this.sessionService.clearData();
-    // Update user Loggedin value on parent (userSession)
-    this.userSession = false;
-    this.eventData.emit(this.userSession);
-    // Redirect to login
+    // Remove browser session and redirect
+    this.sessionService.removeData('user-session');
     this.router.navigateByUrl('/login');
   }
 
