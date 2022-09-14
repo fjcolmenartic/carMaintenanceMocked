f677a99dd423d7f542adc9cbc3dabbe3e4e827fd
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-14 12:07:38 +0200

    Removing unneceary console and services not implemented yet (nor developed)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { SessionStatusService, UserSession } from './services/session-status.service';
+import { Component, OnInit } from '@angular/core';
 import { SessionService } from './services/session.service';
 import { StorageService } from './services/storage.service';
 
@@ -9,9 +7,9 @@ import { StorageService } from './services/storage.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit {
 
-  title = 'carMaintenance';
+  title = 'Car Maintenance';
   // User session on browser
   // To pass data to child navbar and component
   userSession = false;
@@ -19,22 +17,16 @@ export class AppComponent implements OnInit, OnChanges {
   dataSession:any;
   isCheck = '';
 
-  data$: Observable<UserSession>;
-
   constructor(
     private sessionService: SessionService,
-    private sessionStatus: SessionStatusService,
     private storageService: StorageService
     ) {
-      this.data$ = sessionStatus.sessionStatusObservable;
     }
 
   ngOnInit() {
     
     // Check if session is stored on browser (started)
-    let sessionStarted = this.sessionService.getData('user-session');
-    console.log('sessionstarted app component', sessionStarted)
-    
+    let sessionStarted = this.sessionService.getData('user-session');    
 
     // If session starte update userSession value
     if(sessionStarted) {
@@ -42,7 +34,6 @@ export class AppComponent implements OnInit, OnChanges {
     } else {
       this.userSession = false;
     }
-console.log(this.userSession)
 
     let userId = JSON.parse(this.sessionService.getData('user-id') || ' {}');
     userId = userId.toString();
@@ -50,7 +41,6 @@ console.log(this.userSession)
       .subscribe(
       res => {
         this.dataSession = res;
-        console.warn(res)
         this.userName = res.name;
         this.isCheck = 'UPDATE_SUCCESS';
       },
@@ -58,16 +48,10 @@ console.log(this.userSession)
         this.isCheck = 'UPDATE_ERROR';
       });
 
-
-  }
-
-  ngOnChanges() {
-    console.log("%c on changes2", "background:white;color:blue")
   }
 
   childUpdateSession(e: any) {
     this.userSession = e;
-    console.log('update from child... user sesson is ', e)
   }
 
 }
