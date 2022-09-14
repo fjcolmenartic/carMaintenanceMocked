c876bd65211281fcfc024f0d11b19583dd0006c3
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-14 12:05:47 +0200

    WIP new methods to store a global status for user session

--- src/app/services/session-status.service.ts
+++ src/app/services/session-status.service.ts
@@ -1,24 +1,66 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+// import { BehaviorSubject } from 'rxjs';
 
-export interface UserSession {
-  userSessionService: boolean;
+interface UserSession {
+  loggedIn: boolean;
+  userId: number;
+  name: string;
 }
+
+// export interface UserSession {
+//   userSessionService: boolean;
+// }
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionStatusService {
 
-  private _sessionStatusObservable: BehaviorSubject<UserSession> = 
-  new BehaviorSubject<UserSession>({ userSessionService: false });
+  // private _sessionStatusObservable: BehaviorSubject<UserSession> = 
+  // new BehaviorSubject<UserSession>({ userSessionService: false });
 
-  get sessionStatusObservable() {
-    return this._sessionStatusObservable.asObservable();
+  // get sessionStatusObservable() {
+  //   return this._sessionStatusObservable.asObservable();
+  // }
+
+  // set sessionStatusObservableData(data: UserSession) {
+  //   this._sessionStatusObservable.next(data);
+  // }
+
+  user: UserSession = {
+    "loggedIn": false,
+    "userId": 0,
+    "name": ''
   }
 
-  set sessionStatusObservableData(data: UserSession) {
-    this._sessionStatusObservable.next(data);
+  setUserSessionData(userId: number, name:string) {
+    this.user['loggedIn'] = true;
+    this.user['userId'] = userId;
+    this.user['name'] = name;
   }
+
+  getUserSessionData() {
+    return this.user;
+  }
+
+  removeSession() {
+    this.user['loggedIn'] = false;
+    this.user['name'] = '';
+    this.user['userId'] = 0;
+  }
+
+  checkSessionStatus() {
+    let loggedIn = this.user['loggedIn'];
+
+    if(loggedIn) {
+      console.log('SESSION TRUE')
+      return true;
+    } else {
+      console.log('SESSION FALSE')
+      return false;
+    }
+
+  }
+
 
 }
