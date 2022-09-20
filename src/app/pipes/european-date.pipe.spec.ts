24b509631d96a674c35d7236aa40e34722ad82c4
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-20 11:11:09 +0200

    Pipe testing a valid value

--- src/app/pipes/european-date.pipe.spec.ts
+++ src/app/pipes/european-date.pipe.spec.ts
@@ -5,4 +5,14 @@ describe('EuropeanDatePipe', () => {
     const pipe = new EuropeanDatePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('Return a valid value from "2022/09/03" to "03-09-2022"', () => {
+    const pipe = new EuropeanDatePipe();    
+    
+    let date = '2022-09-03';
+    let euDate = pipe.transform(date);
+
+    expect(euDate).toEqual('03/09/2022');
+  });
+
 });
