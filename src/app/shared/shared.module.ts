4d10103641d7286ef039f68b0fc813df697eb5c3
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-20 11:10:42 +0200

    Textbox component removed since removed from project & navigationbar import removed since its imported outside from shared.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ListBoxComponent } from './list-box/list-box.component';
-import { TextBoxComponent } from './text-box/text-box.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    // NavigationBarComponent  
-    ListBoxComponent,
-    TextBoxComponent
+    ListBoxComponent
   ],
   imports: [
     CommonModule,
@@ -17,9 +13,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule
   ],
   exports: [
-    // NavigationBarComponent
-    ListBoxComponent,
-    TextBoxComponent
+    ListBoxComponent
   ]
 })
 
