b07231ffad2005e06e64c57b649b06558848361a
Javier Colmenar <fjcolmenar@serbatic.es>
2022-09-20 18:04:09 +0200

    Testing

--- src/app/content/register/register.component.spec.ts
+++ src/app/content/register/register.component.spec.ts
@@ -41,6 +41,56 @@ describe('RegisterComponent', () => {
 
     expect(spy).toHaveBeenCalled();
 
+  });
+
+  it('On Submit passwords dont match', () => {
+    
+    const button = fixture.debugElement.query(By.css('button'));
+
+    const name = component.registerForm.controls['name'];
+    const email = component.registerForm.controls['email'];
+    const password = component.registerForm.controls['password'];
+    const confirmPassword = component.registerForm.controls['confirmPassword'];
+    const city = component.registerForm.controls['city'];
+
+    name.setValue('nameTest');
+    email.setValue('test@mail.es');
+    password.setValue('123456');
+    confirmPassword.setValue('654321');
+    city.setValue('cityTest');
+
+    fixture.detectChanges();
+    
+    button.nativeElement.click();
+    
+
+    expect(component.isCheck).toEqual('ERROR_PASSWORDS_DONT_MATCH');
+
+  });
+
+  it('On Submit must be triggered from view', () => {
+    
+    const button = fixture.debugElement.query(By.css('button'));
+
+    const name = component.registerForm.controls['name'];
+    const email = component.registerForm.controls['email'];
+    const password = component.registerForm.controls['password'];
+    const confirmPassword = component.registerForm.controls['confirmPassword'];
+    const city = component.registerForm.controls['city'];
+
+    name.setValue('nameTest');
+    email.setValue('test@mail.es');
+    password.setValue('123456');
+    confirmPassword.setValue('123456');
+    city.setValue('cityTest');
+
+    fixture.detectChanges();
+
+    button.nativeElement.click();
+    
+
+    expect(component.registerSuccess).toBeTrue();
+
   })
 
 });
